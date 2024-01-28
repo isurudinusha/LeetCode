39e8db4b7abf144b5330259694b19f92d17bee21
Isuru Dinusha <isurudinusha@gmail.com>
2024-01-28 02:48:58 +0000

    Arrays & Hashing (4 / 9)

--- 49.group-anagrams.py
+++ 49.group-anagrams.py
@@ -7,6 +7,25 @@
 # @lc code=start
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+     
+        d = {}
+        for s in strs:
+            key = ''.join(sorted(s))
+            if key not in d:
+                d[key] = [s]
+            else:
+                d[key].append(s)
+        return list(d.values())
+
+
+        # 2. defaultdict
+        # d = collections.defaultdict(list)
+        # for s in strs:
+        #     key = tuple(sorted(s))
+        #     d[key].append(s)
+        # return list(d.values())
         
+
+
 # @lc code=end
 
--- PlayGround.py
+++ PlayGround.py
@@ -1,5 +1,18 @@
-result = "aacc"
-word = "ccac"
-
-print(sorted(word))
-
+from collections import defaultdict 
+   
+   
+# Defining the dict 
+d = defaultdict(int) 
+   
+L = [1, 2, 3, 4, 2, 4, 1, 2,0] 
+   
+# Iterate through the list 
+# for keeping the count 
+for i in L: 
+       
+    # The default value is 0 
+    # so there is no need to  
+    # enter the key first 
+    d[i] = d[i] + 1
+       
+print(d) 
